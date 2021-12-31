13c8f1f31f8f2e7bcf779779aa2bdefcee753e6c
robonexx <robertwagar@gmail.com>
2021-12-31 11:19:14 +0100

    -

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,5 +1,7 @@
 import './SingleCard.css';
 
+// challenge was to create the component and then add it to the project. had an other css but then copied Shauns
+
 export default function SingleCard({ card }) {
   return (
     <div className='card'>
