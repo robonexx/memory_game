34bd3f91b04ed54bd977edc73961d2dcd65e0e41
robonexx <robertwagar@gmail.com>
2021-12-31 11:18:08 +0100

    Challenge singlecard on this exercise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    // make an array with 2 of the cardImages array with help of spread operator
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      // mapping the cards and setting a unique id with math random
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      {/* creating the grid for the cards and mapping them */}
+      <div className='card-grid'>
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
